12e61cc805e36d1e8ccd8f3aa654caf54f75b47e
EC2 Default User <ec2-user@ip-172-31-8-23.ap-southeast-1.compute.internal>
2018-09-04 10:10:25 +0000

    main design

---
+++ app/javascript/packs/components/carousel.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="carousel carousel-slider center">
+    <div class="carousel-fixed-item center">
+      <a class="btn waves-effect white grey-text darken-text-2">button</a>
+    </div>
+    <div class="carousel_item1 carousel-item black-text" href="#one!">
+      <h2>First Panel</h2>
+      <p class="white-text">This is your first panel</p>
+    </div>
+    <div class="carousel_item2 carousel-item amber white-text" href="#two!">
+      <h2>Second Panel</h2>
+      <p class="white-text">This is your second panel</p>
+    </div>
+    <div class="carousel_item3 carousel-item green white-text" href="#three!">
+      <h2>Third Panel</h2>
+      <p class="white-text">This is your third panel</p>
+    </div>
+    <div class="carousel_item4 carousel-item blue white-text" href="#four!">
+      <h2>Fourth Panel</h2>
+      <p class="white-text">This is your fourth panel</p>
+    </div>
+  </div>
+</template>
+<script>
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
+    var instance = M.Carousel.getInstance(elem);
+  });
+   
+  $(document).ready(function(){
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        fullHeight: true,
+        indicators: true
+    });
+  });
+</script>
---
+++ app/javascript/packs/components/header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <ul id="dropdown" class="dropdown-content menu_el">
+            <li><router-link class="menu_el" to = "/">Top</router-link></li>
+            <li><router-link class="menu_el" to = "/about">About</router-link> </li>
+            <li><router-link class="menu_el" to = "/contact">Contact</router-link></li>
+    </ul>
+    <nav>
+      <div class="nav-wrapper container">
+          <img href="/" class="logo_img brand-logo center" src="/assets/12basket_small.png"></img>
+        <ul class="right hide-on-med-and-down">
+            <li><router-link class="menu_el" to = "/">Top</router-link></li>
+            <li><router-link class="menu_el" to = "/about">About</router-link> </li>
+            <li><router-link class="menu_el" to = "/contact">Contact</router-link></li>
+        </ul>
+        <ul class="right hide-on-large-only">
+          <li>
+            <a class="dropdown-button menu_el" href="#!" data-activates="dropdown">
+              Menu<i class="material-icons right">arrow_drop_down</i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
